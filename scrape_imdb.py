e4207cd42cf38f9f2dbcbfcd827bd3e35435fa11
Tomisin <tomisinjenrola@gmail.com>
2017-05-21 18:20:01 -0230

    Scraped brief movie details and stored in csv file

--- scrape_imdb.py
+++ scrape_imdb.py
@@ -1,32 +1,29 @@
-import csv
+import csv, datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome('../Solve 2048/assets/chromedriver')
 driver.get('http://imdb.com/chart/top')
 
-# movie_details = driver.find_elements_by_css_selector('table.chart > tbody.lister-list > tr')
-# print(type(movie_details))
-
-# movies = driver.find_elements_by_css_selector('.titleColumn > a')
-# for movie in movies:
-# 	# print(movie.text, "\t" , movie.get_attribute("href"))
-# 	print(movie.text, "\t" , )
 
 """
 shallow scrape:
-	name, year, rating
+	name, year, movie_ratings			# use webbrowser or bs4 instead?
 """
 movie_names = driver.find_elements_by_css_selector('td.titleColumn > a')
 movie_years = driver.find_elements_by_class_name('secondaryInfo')
 movie_ratings = driver.find_elements_by_css_selector('td.imdbRating > strong')
 
-with open('movie_details.csv', 'w') as csvfile:
+outputFile = open('movie_details_shallow.csv', 'w', newline='')
+outputWriter = csv.writer(outputFile)
+outputWriter.writerow(['Name', 'Release Year', 'Rating'])
 
 for name, year, rating in zip(movie_names, movie_years, movie_ratings):
-	movie_details = '{:<20} {:>10} {:>20}'.format(name.text, year.text, rating.text)
-	# print(name.text, "\t\t", year.text, "\t\t", rating.text)
-	print(movie_details)
+	outputWriter.writerow([name.text, year.text, rating.text])
+
+outputFile.close()
+driver.close()
+
 
 """
 deep scrape:
